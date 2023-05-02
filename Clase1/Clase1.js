54a23ec8ddd6d69e45726ac610f7530b4710ff37
Diego <dienqn84@gmail.com>
2023-05-02 20:46:54 -0300

    clase1 Backend, tipo de datos en JS

---
+++ Clase1/Clase1.js
@@ -0,0 +1,22 @@
+// vemos tipos de de datos Js
+
+let name = 'Diego'; // String
+let lastname = 'Barros Sofio'; // String
+let age = 38; // number
+let man = true; // bulean
+let esnull = null; // null
+let esundefined = undefined; //undefined
+
+// objeto
+
+let esposa = {
+  name: 'Daniela',
+  age: 35,
+};
+
+// array de objetos
+let peliculas = ['El señor de los Anillos', 'Volver al Futuro', 'El Hobbit'];
+
+console.log(peliculas);
+console.log(esposa);
+console.log(name);
