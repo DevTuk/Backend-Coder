9040201bfdffeced797adf8fa0a3552009f1fc8c
Diego Barros Sofio <89145305+DevTuk@users.noreply.github.com>
2023-05-12 20:48:53 -0300

    Update index.js

--- Desafio01/index.js
+++ Desafio01/index.js
@@ -1,64 +1,59 @@
 class ProductManager {
   constructor() {
     this.products = [];
-    this.nextId = 1;
+    this.nuevoID = 1;
   }
+
   addProduct(title, description, price, thumbnail, code, stock) {
-    //Validar que no se repita el campo “code” y que todos los campos sean obligatorios. Al agregarlo, debe crearse con un id autoincrementable
-    const productoExistente = this.products.find(
+    const productoExiste = this.products.find(
       (producto) => producto.code === code
     );
-    //hacemos referencia a products, y con find buscamos que cumpla con determinada condición. En este caso "code".
-    //Entonces si code existe en el array de products, el producto que quiero agregar debería mostrar un error de que ya existe.
-    if (productoExistente) {
+    if (productoExiste) {
       console.log(
-        `Producto "${title}" tiene un error, el codigo "${code}" es el mismo del producto "${productoExistente.title}".`
+        `El producto ${title} tiene un error, el codigo ${code} es el mismo de otro producto existente ${productoExiste.title}"`
       );
       return;
     }
-    //verificamos si todos los campos obligatorios fueron proporcionados
+
     if (!title || !description || !price || !thumbnail || !code || !stock) {
       console.log(
-        `Todos los campos son obligatorios en el producto "${title}" que estas intentando ingresar`
+        `Todos los campos son obligatorios en el producto ${title} que estas intentando ingresar`
       );
-      return;
-    } //creamos el nuevo producto
-    const producto = {
-      id: this.nextId++,
-      title,
-      description,
-      price,
-      thumbnail,
-      code,
-      stock,
-    };
-    //si no existe, entonces agregamos el producto con los campos que debe reunir de forma obligatoria.
-    this.products.push(producto);
-    console.log(`El producto ${producto.id} fue agregado correctamente`);
+    } else {
+      const producto = {
+        id: this.nuevoID++,
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+      };
+      this.products.push(producto);
+      {
+        console.log(`el producto ${producto.title} fue agregado correctamente`);
+      }
+    }
   }
-  getProducts() {
-    // debe devolver el arreglo con todos los productos creados hasta ese momento
+  getProduct() {
     return this.products;
   }
   getProductById(id) {
-    const producto = this.products.find((producto) => producto.id === id);
-    //buscamos en products que el id ingresado exista
-    // En caso de no coincidir ningún id, mostrar en consola un error “Not found”
-    if (!producto) {
-      console.log(`“Not found” El producto con el id ${id} no existe`);
-      return;
-    }
-    else {
-    console.log(`El producto con el id ${id} fue encontrado`);
-    return producto;
+    const productoId = this.products.find((producto) => producto.id === id);
+    {
+      if (!productoId) {
+        console.log(`"Not Found" el Id n° "${id}" de producto no existe`);
+      } else {
+        console.log(`El producto con el id "${id}" fue encontrado`);
+        return productoId;
+      }
     }
   }
 }
-const productos = new ProductManager();
-productos.addProduct('titulo1', 'descripcion1', 350, 'imagen1', 'abc123', 2);
-productos.addProduct('titulo2', 'descripcion2', 450, 'imagen2', 'abc124', 5);
-productos.addProduct('titulo3', 'descripcion3', 450, 'imagen3', 'abc125', 1);
-productos.addProduct('titulo4', 'descripcion4', 450, 'imagen4', 'abc123', 4);
-productos.addProduct('titulo5', '', 450, 'imagen4', 'abc126', 4);
-console.log(productos.getProducts());
-productos.getProductById(5);
+
+const producto = new ProductManager();
+producto.addProduct('titulo1', 'descripcion1', 200, 'imagen1', 'code1', 2);
+producto.addProduct('titulo2', 'descripcion1', 200, 'imagen1', 'code2', 2);
+producto.addProduct('titulo3', 'descripcion1', 200, 'imagen1', 'code3', 2);
+console.log(producto.getProduct());
+producto.getProductById(5);
